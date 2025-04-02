834c7a201f57877da8dd7f26fceff5da2d73aadd
NirajDX <niraj.t@designersx.com>
2025-04-02 15:39:12 +0530

    [AP] Folder structure create (AP v1.1.2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../src/Components/Home/Home'
+
 
 function App() {
- 
+
 
   return (
     <>
-    
-    
-    
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
