af03697f51cd3265ea012db68e8060dca294fa30
vinodchris <vinod09.dx@gmail.com>
2025-04-04 18:03:00 +0530

    vk changes working on homepage v.3

--- src/Components/Home/ProjectDelivery/ProjectDelivery.jsx
+++ src/Components/Home/ProjectDelivery/ProjectDelivery.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './ProjectDelivery.module.css';
+import { Link } from 'react-router-dom';
 
 
 const orders = [
     {
       id: 1,
-      title: "Davis Chairs",
+      title: "Davis Chairss",
       etd: "18-4-2025",
       eta: "25-4-2025",
       status: "Installed",
-      statusColor: "text-green-500",
-      progressColor: "bg-green-500",
+      statusColor: "LinearGreen",
+      progressColor: "Green",
+      progressWidth: "100%",
+      bgColor: "#4CAF50",
+      image: "/Images/PicPro1.png",
       comment: {
         user: "John Vick",
         time: "13 MAR 03:45 PM",
@@ -24,11 +28,57 @@ const orders = [
       etd: "12-4-2025",
       eta: "20-4-2025",
       status: "Delivered",
-      statusColor: "text-orange-500",
-      progressColor: "bg-orange-500",
+      statusColor: "FEAD37",
+      progressColor: "#FEAD37",
+      progressWidth: "50%",
       comment: null,
       postedAgo: "Yesterday",
+      bgColor: "#FEAD37",
     },
+    {
+        id: 3,
+        title: "Andreu World",
+        etd: "10-4-2025",
+        eta: "15-4-2025",
+        status: "In Transit",
+        statusColor: "LinearGreen",
+        progressColor: "#6C35B1",
+        bgColor: "#6C35B1",
+        progressWidth: "60%",
+        image: "/Images/PicPro1.png",
+        comment: {
+          user: "Kate Nick",
+          time: "13 MAR 03:45 PM",
+          message: "We are assembling",
+          postedAgo: "2 days ago",
+        },
+      },
+      {
+        id: 4,
+        title: "Berhnhardt",
+        etd: "15-4-2025",
+        eta: "22-4-2025",
+        status: "Arrived",
+        statusColor: "text-orange-500",
+        progressColor: "#004A84",
+        progressWidth: "60%",
+        comment: null,
+        postedAgo: "5 days ago",
+        bgColor: "#004A84",
+      },
+      {
+        id: 5,
+        title: "Herman Miller",
+        etd: "11-4-2025",
+        eta: "19-4-2025",
+        status: "Order Processed",
+        statusColor: "#FF5E00",
+        progressColor: "#FF5E00",
+        progressWidth: "25%",
+        comment: null,
+        postedAgo: "05 MAR 03:45 PM",
+        bgColor: "#FF5E00",
+      },
   ];
 
 function ProjectDelivery() {
@@ -42,37 +92,47 @@ function ProjectDelivery() {
     <h4>Project Delivery Update</h4>
     <button className={styles.button}>View All</button>
   </div>
+  
 
   <div className={styles.Container}>
       {orders.map((order) => (
+
+<Link  to={`/order/${order.id}`}
+  key={order.id}
+  state={{ order }} // Pass the order object
+  className={styles.linkStyle}>
         <div key={order.id} className={styles.orderCard}>
           {/* Header */}
           <div className={styles.orderHeader}>
             <h2 className={styles.orderTitle}>{order.title}</h2>
-            <span className={`${styles.orderStatus} ${order.statusColor}`}>
-              {order.status}
+            <span className={`${styles.orderStatus} ${order.statusColor}`} 
+              style={{ color: order.bgColor }}
+            >
+              {order.status}<span className={styles.LineColor} style={{ backgroundColor: order.bgColor }} ></span>
             </span>
           </div>
 
           {/* ETD & ETA */}
           <p className={styles.orderDetails}>
-            <strong>ETD:</strong> {order.etd} | <strong>ETA:</strong> {order.eta}
+            <strong>ETD :</strong> {order.etd} | <strong>ETA :</strong> {order.eta}
           </p>
 
           {/* Comment Section */}
           {order.comment && (
             <div className={styles.commentBox}>
-              <p className={styles.commentUser}>{order.comment.user}</p>
+              <div className={styles.commentHeader}>
+              <p className={styles.commentUser}><img src={order.image}  alt="Comment" className={styles.PicImg} /> {order.comment.user}</p>
               <p className={styles.commentTime}>{order.comment.time}</p>
+              </div>
               <p className={styles.commentMessage}>{order.comment.message}</p>
             </div>
           )}
 
           {/* Footer */}
           <div className={styles.orderFooter}>
-            <span>{order.comment ? order.comment.postedAgo : order.postedAgo}</span>
+            <span className={styles.TimeFlex}><img src="/Svg/TimeIcon.svg"/> {order.comment ? order.comment.postedAgo : order.postedAgo}</span>
             <button className={styles.addComment}>
-              <img src="/comment-icon.png" alt="Comment" />
+              <img src="/Svg/CommentIcon.svg" alt="Comment" />
               Add Comment
             </button>
           </div>
@@ -81,10 +141,11 @@ function ProjectDelivery() {
           <div className={styles.progressBar}>
             <div
               className={`${styles.progress} ${order.progressColor}`}
-              style={{ width: order.progressWidth }}
+              style={{ width: order.progressWidth, backgroundColor: order.progressColor }}
             ></div>
           </div>
         </div>
+        </Link>
       ))}
     </div>
 
