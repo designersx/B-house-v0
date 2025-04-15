3989db8c046e5f387c12484ea70fafbc0c5c1969
NirajDX <niraj.t@designersx.com>
2025-04-15 16:30:24 +0530

    [AP] orderinfo or punchlist detail page impolement (AP v.1.1.3)

--- src/Components/HeaderTab/HeaderTab.jsx
+++ src/Components/HeaderTab/HeaderTab.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import styles from '../HeaderTab/HeaderTab.module.css';
 import { useNavigate } from 'react-router-dom';
 import ModalSearch from '../ModalSearch/ModalSearch';
+import OffCanvas from '../OffCanvas/OffCanvas';
 
 const HeaderTab = ({ title, subtitle }) => {
     const navigate = useNavigate();
     const [showModalSearch, setShowModalSearch] = useState(false);
+    const [showCanvas, setShowCanvas] = useState(false);
+
     return (
-        <div className={styles.headerMain}>
+        <>
+         <div className={styles.headerMain}>
             <div className={styles.titleDiv}>
                 <div onClick={() => navigate(-1)} style={{ cursor: "pointer" }}>
                     <img src='Svg/back-arrow.svg' alt='Back' />
@@ -22,18 +26,33 @@ const HeaderTab = ({ title, subtitle }) => {
                 <div className={styles.iconSearch} onClick={() => setShowModalSearch(true)}>
                     <img src='Svg/searchSvg.svg' alt='Search' />
                 </div>
-                <div className={styles.iconFillter}>
+                <div className={styles.iconFillter} onClick={() => setShowCanvas(true)}>
                     <img src='Svg/filterSvg.svg' alt='Filter' />
                 </div>
             </div>
-        
 
-            <ModalSearch isOpen={showModalSearch}
+            <ModalSearch
+                isOpen={showModalSearch}
                 onClose={() => setShowModalSearch(false)}
                 minHeight="30%"
             />
-        </div>
 
+           
+        </div>
+        <OffCanvas
+                isOpen={showCanvas}
+                onClose={() => setShowCanvas(false)}
+                direction="right"
+                width="80%"
+                overlay={true}
+            >
+                <div style={{ padding: '20px' }}>
+                    <h3>Filters</h3>
+                    <p>filter </p>
+                </div>
+            </OffCanvas>
+        </>
+       
     );
 };
 
