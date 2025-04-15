83253d4751764b2b182420b96d806fcd5cffc571
NirajDX <niraj.t@designersx.com>
2025-04-15 18:00:21 +0530

    [AP] orderinfo or punchlist detail page impolement (AP v.1.1.6)

--- src/Components/Punchlist/PunchSlider.jsx
+++ src/Components/Punchlist/PunchSlider.jsx
@@ -1,52 +1,76 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import styles from "./SliderPunch.module.css";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-// import styles from "./Punchlist.module.css";
-
 export default function SimpleSlider() {
-    const settings = {
-        dots: false ,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
-      };
-  return (
-    <div className={` ${styles.PunchImageSlider} PunchImageSlider `}>
-    <Slider {...settings}>
-      <div className={styles.slideImg}>
-       <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-      <div  className={styles.slideImg}>
-      <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-      <div  className={styles.slideImg}>
-      <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-      <div  className={styles.slideImg}>
-      <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-      <div  className={styles.slideImg}>
-      <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-      <div  className={styles.slideImg}>
-      <img src="/Images/ChairImg.png" alt="Slider Image 1" />
-      </div>
-    </Slider>
+  const sliderRef = useRef(null);
 
-{/* <Slider {...settings}>
-        <div className={styles.slide}>Slide 1</div>
-        <div className={styles.slide}>Slide 2</div>
-        <div className={styles.slide}>Slide 3</div>
-        <div className={styles.slide}>Slide 4</div>
-        <div className={styles.slide}>Slide 5</div>
-      </Slider> */}
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "40px", 
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 376,
+        settings: {
+          centerPadding: "20px",
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 2, 
+          centerPadding: "25px",
+        },
+      },
+    ],
+  };
+
+  const nextSlide = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const prevSlide = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  return (
+    <div className={styles.PunchImageSlider}>
+      <button className={`${styles.arrow} ${styles.left}`} onClick={prevSlide}>
+        <img src="Svg/leftArrow.svg" alt="Previous Slide" />
+      </button>
+      <button className={`${styles.arrow} ${styles.right}`} onClick={nextSlide}>
+        <img src="Svg/rightArrow.svg" alt="Next Slide" />
+      </button>
+      <Slider ref={sliderRef} {...settings}>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 1" />
+        </div>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 2" />
+        </div>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 3" />
+        </div>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 4" />
+        </div>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 5" />
+        </div>
+        <div className={styles.slideImg}>
+          <img src="/Images/ChairImg.png" alt="Slider Image 6" />
+        </div>
+      </Slider>
     </div>
   );
 }
