7977b742a56e2821dcb0f99a34f3ef5b61922459
NirajDX <niraj.t@designersx.com>
2025-04-17 11:02:31 +0530

    [AP] slick setting change (AP v.1.1.1)

--- src/Components/Punchlist/PunchSlider.jsx
+++ src/Components/Punchlist/PunchSlider.jsx
@@ -3,7 +3,8 @@ import Slider from "react-slick";
 import styles from "./SliderPunch.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { url2 } from "../../config/url"; 
+import { url2 } from "../../config/url";
+
 export default function SimpleSlider({ images = [] }) {
   const sliderRef = useRef(null);
 
@@ -32,29 +33,55 @@ export default function SimpleSlider({ images = [] }) {
   const nextSlide = () => sliderRef.current.slickNext();
   const prevSlide = () => sliderRef.current.slickPrev();
 
+  const showSlider = images.length >= 3;
+
   return (
     <div className={styles.PunchImageSlider}>
-      <button className={`${styles.arrow} ${styles.left}`} onClick={prevSlide}>
-        <img src="Svg/leftArrow.svg" alt="Previous Slide" />
-      </button>
+      {showSlider ? (
+        <>
+          <button
+            className={`${styles.arrow} ${styles.left}`}
+            onClick={prevSlide}
+          >
+            <img src="Svg/leftArrow.svg" alt="Previous Slide" />
+          </button>
 
-      <button className={`${styles.arrow} ${styles.right}`} onClick={nextSlide}>
-        <img src="Svg/rightArrow.svg" alt="Next Slide" />
-      </button>
+          <button
+            className={`${styles.arrow} ${styles.right}`}
+            onClick={nextSlide}
+          >
+            <img src="Svg/rightArrow.svg" alt="Next Slide" />
+          </button>
 
-      <Slider ref={sliderRef} {...settings}>
-        {images.slice(0, 5).map((img, i) => (
-          <div className={styles.slideImg} key={i}>
-            <a
-              href={`${url2}/${img}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={`${url2}/${img}`} alt={`Image ${i + 1}`} />
-            </a>
-          </div>
-        ))}
-      </Slider>
+          <Slider ref={sliderRef} {...settings}>
+            {images.slice(0, 5).map((img, i) => (
+              <div className={styles.slideImg} key={i}>
+                <a
+                  href={`${url2}/${img}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={`${url2}/${img}`} alt={`Image ${i + 1}`} />
+                </a>
+              </div>
+            ))}
+          </Slider>
+        </>
+      ) : (
+        <div className={styles.flexImages}>
+          {images.map((img, i) => (
+            <div className={styles.slideImg} key={i}>
+              <a
+                href={`${url2}/${img}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={`${url2}/${img}`} alt={`Image ${i + 1}`} />
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
