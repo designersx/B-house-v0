dda2facce0593bce8a979653cddfc84fafd25bab
NirajDX <niraj.t@designersx.com>
2025-04-22 15:47:34 +0530

    [AP] Ui bug fix (AP v.1.1.3)

--- src/Components/Invoice/Chart/Chart.jsx
+++ src/Components/Invoice/Chart/Chart.jsx
@@ -107,7 +107,16 @@ const Chart = () => {
                         <p className={styles.label}>Paid Amt.</p>
                     </div>
                 </div>
+          <div className={styles.cashflowMessage}>
+            <img src='Svg/cashFlow.svg' alt=''/>
+           <div className={styles.cashFlowTextMain }>
+            <p className={styles.cashflowTitle }><b>Cashflow Looks Good!</b></p>
+            <p className={styles.cashflowText}>You're managing it well, stay consistent!</p>
+           </div>
+
+          </div>
             </div>
+          
         </div>
     );
 };
