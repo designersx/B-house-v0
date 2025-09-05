8458262ec94db4d7b9835899546b2a156e58e895
sHivAM-889 <shivamrana.dx@gmail.com>
2025-09-05 16:41:53 +0530

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 
-const URL = "https://bhouse-stag.truet.net/api"; 
-// const URL = "https://bhouse.truet.net/api";
+// const URL = "https://bhouse-stag.truet.net/api"; 
+const URL = "https://bhouse.truet.net/api";
 // const URL = "http://localhost:8587/api"; 
 export default  URL;
